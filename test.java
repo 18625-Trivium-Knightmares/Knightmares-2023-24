19b4cde483314d8946f1586b4637f0fa0420eda9
Jacøb <85955074+cuber1515@users.noreply.github.com>
2023-10-12 15:34:17 -0700

    Add files via upload

--- test.java
+++ test.java
@@ -4,52 +4,71 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class test extends LinearOpMode {
-    DcMotor FR, FL, BR, BL, A, H;
+    DcMotor FR, FL, BR, BL, actu, hoi, ch;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        FR = hardwareMap.get(DcMotorEx.class, "Front Right");
-        FL = hardwareMap.get(DcMotorEx.class, "Front Left");
-        BR = hardwareMap.get(DcMotorEx.class, "Back Right");
-        BL = hardwareMap.get(DcMotorEx.class, "Back Left");
+        FR = hardwareMap.dcMotor.get("rightFront");
+        FL = hardwareMap.dcMotor.get("leftFront");
+        BR = hardwareMap.dcMotor.get("rightBack");
+        BL = hardwareMap.dcMotor.get("leftBack");
 
-        BR.setDirection((DcMotor.Direction.REVERSE));
         FR.setDirection((DcMotor.Direction.REVERSE));
+        FL.setDirection((DcMotor.Direction.REVERSE));
+        BR.setDirection((DcMotor.Direction.REVERSE));
 
-        A = hardwareMap.get(DcMotorEx.class, "Linear Actuator");
-        H = hardwareMap.get(DcMotorEx.class, "Hoist Arm");
+        actu = hardwareMap.dcMotor.get("actuator");
+        hoi = hardwareMap.dcMotor.get("hoist");
+        ch = hardwareMap.dcMotor.get("ch");
 
 
         waitForStart();
 
         while(opModeIsActive()) {
+            FR.setPower(gamepad1.right_stick_y);
+            FL.setPower(gamepad1.right_stick_y);
+            BR.setPower(gamepad1.left_stick_y);
+            BL.setPower(gamepad1.left_stick_y);
+
             if (gamepad1.a) {
-                A.setPower(0.5);
-                sleep(100);
-                A.setPower(0);
+                actu.setPower(1);
+            } else if (!gamepad1.a) {
+                actu.setPower(0);
             }
 
             if (gamepad1.b) {
-                A.setPower(-0.5);
-                sleep(100);
-                A.setPower(0);
+                actu.setPower(-1);
+            } else if (!gamepad1.b) {
+                actu.setPower(0);
             }
+
             if (gamepad1.dpad_down) {
-                H.setPower(0.5);
-                sleep(100);
-                H.setPower(0);
+                hoi.setPower(0.5);
+            } else if (!gamepad1.dpad_down) {
+                hoi.setPower(0);
             }
 
             if (gamepad1.dpad_up) {
-                H.setPower(-0.5);
-                sleep(100);
-                H.setPower(0);
+                hoi.setPower(-0.5);
+            } else if (!gamepad1.dpad_up) {
+                hoi.setPower(0);
+            }
+
+            if (gamepad1.y) {
+                ch.setPower(0.5);
+            } else if (!gamepad1.y) {
+                ch.setPower(0);
+            }
+
+            if (gamepad1.x) {
+                ch.setPower(-0.5);
+            } else if (!gamepad1.x) {
+                ch.setPower(0);
             }
         }
     }
-
 }
