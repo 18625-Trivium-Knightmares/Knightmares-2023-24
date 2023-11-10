c48ed983b8ff653d9cbfddf1124314013be173bc
LoganLemay <112989231+LoganLemay@users.noreply.github.com>
2023-11-10 09:44:50 -0700

    Update teleOpCodeLogan.java
    Started to work on encoders
    You might need to check if I did the slide motor encoder properly
    I am not finished with the chain motor encoder though(I need to find the tick number)

--- teleOp/teleOpCodeLogan.java
+++ teleOp/teleOpCodeLogan.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
-@TeleOp(name = "Tank Drive")
+    @TeleOp(name = "Tank Drive")
 
 public class teleOpCodeLogan extends LinearOpMode {
     /**
@@ -15,11 +15,13 @@ public class teleOpCodeLogan extends LinearOpMode {
      */
 
     DcMotor FR, FL, BR, BL, AM, HM, CM, SM; // All of the motors
+        double ticks = 384.5;
+        double ticks2 = 444;
+        double newTarget;
     Servo Claw;
 
-    double speed = 0.8;
+    double speed = 1;
     double halfspeed = 0.5; //This was for the faster moving parts (Chain motor)
-    double fourthspeed = 0.25;
     double openClaw = 0.001;
     double closeClaw = 1;
     @Override
@@ -33,7 +35,6 @@ public class teleOpCodeLogan extends LinearOpMode {
         HM = hardwareMap.dcMotor.get("hoist");
         AM = hardwareMap.dcMotor.get("actuator");
         SM = hardwareMap.dcMotor.get("Slide");
-
         Claw = hardwareMap.servo.get("Claw");
 
         FR.setDirection((DcMotorSimple.Direction.REVERSE));
@@ -41,6 +42,8 @@ public class teleOpCodeLogan extends LinearOpMode {
 
         telemetry.addData(">", "Press Play to start op mode"); // Will add stuff to the driver hub screen
         telemetry.update(); // Will update the driver hub screen so that the above will appear
+        CM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        SM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         waitForStart(); // When the start button is pressed
 
         if (opModeIsActive()) {
@@ -52,28 +55,22 @@ public class teleOpCodeLogan extends LinearOpMode {
                 // Basic robot moving controls
                 FR.setPower(gamepad1.right_stick_y * speed);
                 BR.setPower(gamepad1.right_stick_y * speed);
-                FL.setPower(gamepad1.left_stick_y);
+                FL.setPower(gamepad1.left_stick_y * speed);
                 BL.setPower(gamepad1.left_stick_y * speed);
 
                 if (gamepad1.dpad_up){
-                    AM.setPower(1);
+                    AM.setPower(0.5);
+                }
+                else if(gamepad1.dpad_down){
+                    AM.setPower(-0.5);
                 }
                 else{
                     AM.setPower(0);
                 }
-                if (gamepad1.dpad_down){
-                    AM.setPower(-1);
-                }
-                else{
-                    AM.setPower(0);
-                }
-                if (gamepad1.y){
+                if (gamepad1.dpad_left){
                     HM.setPower(0.5);
                 }
-                else{
-                    HM.setPower(0);
-                }
-                if (gamepad1.a){
+                else if (gamepad1.dpad_right){
                     HM.setPower(-0.5);
                 }
                 else{
@@ -84,13 +81,13 @@ public class teleOpCodeLogan extends LinearOpMode {
                     FR.setPower(-speed);
                     BL.setPower(speed);
                     BR.setPower(speed);
-                    FL.setPower(-1);
+                    FL.setPower(-speed);
                 }
                 else if(gamepad1.left_trigger>0){
                     FR.setPower(speed);
                     BL.setPower(-speed);
                     BR.setPower(-speed);
-                    FL.setPower(1);
+                    FL.setPower(speed);
                 }
                 else{
                     FR.setPower(0);
@@ -99,18 +96,52 @@ public class teleOpCodeLogan extends LinearOpMode {
                     FL.setPower(0);
                 }
                 /**
-                 * ALL UNDER GAMEPAD 2
+                 * ALL USED TO BE UNDER GAMEPAD 2
                  */
-                CM.setPower(gamepad2.right_stick_y * halfspeed);
-                SM.setPower(gamepad2.left_stick_y * -fourthspeed);
-                if(gamepad2.left_trigger>0){
-                    Claw.setPosition(openClaw);
+                //CM.setPower(gamepad2.right_stick_y*halfspeed; THESE ARE THE OLD GAMEPAD 2 CONTROLS
+                if(gamepad1.b){
+                    CM.setPower(halfspeed);
                 }
-                if(gamepad2.right_trigger>0){
-                    Claw.setPosition(closeClaw);
+                else if(gamepad1.x){
+                    CM.setPower(-halfspeed);
                 }
+                else{
+                    CM.setPower(0);
+                }
+                //SM.setPower(gamepad2.left_stick_y * -speed); THESE ARE THE  OLD GAMEPAD 2 CONTROLS
+                if(gamepad1.y) {
+                    encoder(2);
 
+                }
+            }
+                if(gamepad1.a){
+                    SM.setPower(-speed);
+                }
+                else{
+                    SM.setPower(0);
+                }
+            if(gamepad1.left_bumper){         //gamepad1.left_trigger > 0 OLD GAMEPAD 2 CONTROLS
+                Claw.setPosition(openClaw);
+            }
+            if(gamepad1.right_bumper){       //gamepad1.right_bumper > 0  OLD GAMEPAD 2 CONTROLS
+                Claw.setPosition(closeClaw);
             }
         }
     }
-}
+        public void encoder(int turnage){
+            newTarget = ticks/turnage;
+            SM.setTargetPosition((int)newTarget);
+            SM.setPower(speed);
+            SM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+
+        public void encoder2(int turnage2){
+            newTarget = ticks2/turnage2;
+            CM.setTargetPosition((int)newTarget);
+            CM.setPower(speed);
+            CM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+    }
+//CM ENCODER!!!!!!!!!
+
+
