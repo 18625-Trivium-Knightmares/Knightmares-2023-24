d32a7f8270459e51504df7f058c285d2faaa9948
Jacøb <85955074+cuber1515@users.noreply.github.com>
2023-11-15 15:09:16 -0700

    updated color detection test stuff

--- colorDetectionAuto.java
+++ colorDetectionAuto.java
@@ -23,7 +23,7 @@ public class colorDetectionAuto extends OpMode {
 
     @Override
     public void init() {
-        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam");
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "name");
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
 
@@ -50,51 +50,41 @@ public class colorDetectionAuto extends OpMode {
     class colorPipeline extends OpenCvPipeline {
         Mat YCbCr = new Mat();
         Mat leftCrop;
-        Mat midCrop;
         Mat rightCrop;
         double leftavgfin;
-        double midavgfin;
         double rightavgfin;
         Mat outPut = new Mat();
-        Scalar rectColor = new Scalar(255.0, 0.0, 0.0);
+        Scalar rectColor = new Scalar(0.0, 0.0, 255.0);
 
         public Mat processFrame(Mat input) {
             Imgproc.cvtColor(input, YCbCr, Imgproc.COLOR_RGB2YCrCb);
             telemetry.addLine("pipeline running");
 
-            Rect leftRect = new Rect(1, 1, 212, 359);
-            Rect midRect = new Rect(214, 1, 212, 359);
-            Rect rightRect = new Rect(427, 1, 212, 359);
+            Rect leftRect = new Rect(1, 1, 300, 359);
+            Rect rightRect = new Rect(339, 1, 300, 359);
 
             input.copyTo(outPut);
             Imgproc.rectangle(outPut, leftRect, rectColor, 2);
-            Imgproc.rectangle(outPut, midRect, rectColor, 2);
             Imgproc.rectangle(outPut, rightRect, rectColor, 2);
 
             leftCrop = YCbCr.submat(leftRect);
-            midCrop = YCbCr.submat(midRect);
             rightCrop = YCbCr.submat(rightRect);
 
             Core.extractChannel(leftCrop, leftCrop, 2);
-            Core.extractChannel(midCrop, midCrop, 2);
             Core.extractChannel(rightCrop, rightCrop, 2);
 
             Scalar leftavg = Core.mean(leftCrop);
-            Scalar midavg = Core.mean(midCrop);
             Scalar rightavg = Core.mean(rightCrop);
 
             leftavgfin = leftavg.val[0];
-            midavgfin = midavg.val[0];
             rightavgfin = rightavg.val[0];
 
-            if (rightavgfin > leftavgfin && rightavgfin > midavgfin){
-                telemetry.addLine("It is on the right side");
-            }
-            if (midavgfin > rightavgfin && midavgfin > leftavgfin) {
-                telemetry.addLine("It is in the middle");
-            }
-            if (leftavgfin > rightavgfin && leftavgfin > midavgfin) {
+            if (leftavgfin > rightavgfin) {
                 telemetry.addLine("It is on the left side");
+            } else if (rightavgfin > leftavgfin) {
+                telemetry.addLine("It is on the right side");
+            } else {
+                telemetry.addLine("It must be in the middle");
             }
 
             return outPut;
