df11f924f675a6ad7c4a6c4fb588156f81627a39
LoganLemay <112989231+LoganLemay@users.noreply.github.com>
2023-11-16 15:40:25 -0700

    Update teleOpCodeLogan.java
    We switched once again from one controller to two controllers

--- teleOp/teleOpCodeLogan.java
+++ teleOp/teleOpCodeLogan.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.ColorSensor;
+
 @TeleOp(name = "Tank Drive")
 
 public class teleOpCodeLogan extends LinearOpMode {
@@ -16,9 +16,10 @@ public class teleOpCodeLogan extends LinearOpMode {
 
     DcMotor FR, FL, BR, BL, AM, HM, CM, SM; // All of the motors
     double ticks = 384.5;
-    double ticks2 = 537.7;
+    double ticks2 = 444;
     double newTarget;
     Servo Claw;
+
 
     double speed = 1;
     double halfspeed = 0.5; //This was for the faster moving parts (Chain motor)
@@ -28,9 +29,9 @@ public class teleOpCodeLogan extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // Assigning all of the servos and motors
         FR = hardwareMap.dcMotor.get("rightFront");
-        FL = hardwareMap.dcMotor.get("leftFront");
+        FL = hardwareMap.dcMotor.get("Buh");
         BR = hardwareMap.dcMotor.get("rightBack");
-        BL = hardwareMap.dcMotor.get("leftBack");
+        BL = hardwareMap.dcMotor.get("Bruh");
         CM = hardwareMap.dcMotor.get("ch");
         HM = hardwareMap.dcMotor.get("hoist");
         AM = hardwareMap.dcMotor.get("actuator");
@@ -38,7 +39,7 @@ public class teleOpCodeLogan extends LinearOpMode {
         Claw = hardwareMap.servo.get("Claw");
 
         FR.setDirection((DcMotorSimple.Direction.REVERSE));
-        BR.setDirection((DcMotorSimple.Direction.REVERSE));
+        //BR.setDirection((DcMotorSimple.Direction.REVERSE));
 
         telemetry.addData(">", "Press Play to start op mode"); // Will add stuff to the driver hub screen
         telemetry.update(); // Will update the driver hub screen so that the above will appear
@@ -54,9 +55,9 @@ public class teleOpCodeLogan extends LinearOpMode {
                  */
                 // Basic robot moving controls
                 FR.setPower(gamepad1.right_stick_y * speed);
-                BR.setPower(gamepad1.right_stick_y * speed);
+                BR.setPower(gamepad1.right_stick_y * (speed*0.75));
                 FL.setPower(gamepad1.left_stick_y * speed);
-                BL.setPower(gamepad1.left_stick_y * speed);
+                BL.setPower(gamepad1.left_stick_y * (speed*2));
 
                 if (gamepad1.dpad_up) {
                     AM.setPower(0.5);
@@ -67,63 +68,66 @@ public class teleOpCodeLogan extends LinearOpMode {
                 }
                 if (gamepad1.dpad_left) {
                     HM.setPower(0.5);
-//                    encoder2(0.5);
                 } else if (gamepad1.dpad_right) {
                     HM.setPower(-0.5);
-//                    encoder2(0);
                 } else {
                     HM.setPower(0);
                 }
                 //This is for strafing
+                //THESE ARE THE OLD GAMEPAD 2 CONTROLS
                 if (gamepad1.right_trigger > 0) {
                     FR.setPower(-speed);
                     BL.setPower(speed);
                     BR.setPower(speed);
                     FL.setPower(-speed);
-                } else if (gamepad1.left_trigger > 0) {
-                    FR.setPower(speed);
-                    BL.setPower(-speed);
-                    BR.setPower(-speed);
-                    FL.setPower(speed);
                 } else {
-                    FR.setPower(0);
-                    BL.setPower(0);
-                    BR.setPower(0);
-                    FL.setPower(0);
+                    if (gamepad1.left_trigger > 0) {
+                        FR.setPower(speed);
+                        BL.setPower(-speed);
+                        BR.setPower(-speed);
+                        FL.setPower(speed);
+                    } else {
+                        FR.setPower(0);
+                        BL.setPower(0);
+                        BR.setPower(0);
+                        FL.setPower(0);
+                    }
                 }
+                CM.setPower(gamepad2.right_stick_y*halfspeed); //THESE ARE THE OLD GAMEPAD 2 CONTROLS
                 /**
-                 * ALL USED TO BE UNDER GAMEPAD 2
+                 * ALL UNDER GAMEPAD 2
                  */
-                //CM.setPower(gamepad2.right_stick_y*halfspeed; THESE ARE THE OLD GAMEPAD 2 CONTROLS
-                if (gamepad1.b) {
+                /**if (gamepad1.b) {
                     CM.setPower(halfspeed);
                 } else if (gamepad1.x) {
                     CM.setPower(-halfspeed);
                 } else {
                     CM.setPower(0);
-                }
-                //SM.setPower(gamepad2.left_stick_y * -speed); THESE ARE THE  OLD GAMEPAD 2 CONTROLS
-                if (gamepad1.y) {
-                    SM.setPower(speed);
+                }**/
+                SM.setPower(gamepad2.left_stick_y * -speed); //THESE ARE THE  OLD GAMEPAD 2 CONTROLS
+                /**if (gamepad1.y) {
+                    encoder(2);
+//                    SM.setPower(speed);
                 }
 
                 if (gamepad1.a) {
                     SM.setPower(-speed);
                 } else {
                     SM.setPower(0);
-                }
-                if (gamepad1.left_bumper) {         //gamepad1.left_trigger > 0 OLD GAMEPAD 2 CONTROLS
+                }**/
+                if (gamepad2.left_bumper) {         //gamepad1.left_trigger > 0 OLD GAMEPAD 2 CONTROLS
                     Claw.setPosition(openClaw);
                 }
-                if (gamepad1.right_bumper) {       //gamepad1.right_bumper > 0  OLD GAMEPAD 2 CONTROLS
+                if (gamepad2.right_bumper) {       //gamepad1.right_bumper > 0  OLD GAMEPAD 2 CONTROLS
                     Claw.setPosition(closeClaw);
                 }
             }
         }
     }
-    /*public void encoder(int turnage){
+    public void encoder(int a){
+//        SM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         SM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        newTarget = ticks/turnage;
+        newTarget = ticks/a;
         SM.setTargetPosition((int)newTarget);
         SM.setPower(speed);
         SM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -133,16 +137,15 @@ public class teleOpCodeLogan extends LinearOpMode {
         SM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
-    public void encoder2(double turnage2){
-        HM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        newTarget = ticks2*turnage2;
-        HM.setTargetPosition((int)newTarget);
-        HM.setPower(speed);
-        HM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        while (HM.isBusy()) {
+    public void encoder2(int a){
+        newTarget = ticks2/a;
+        CM.setTargetPosition((int)newTarget);
+        CM.setPower(speed);
+        CM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while(CM.isBusy()){
         }
-        HM.setPower(0);
-        HM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    }*/
+        CM.setPower(0);
+        CM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
 }
 //CM ENCODER!!!!!!!!!
