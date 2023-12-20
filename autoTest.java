566503947632619fbde2e0a650e6edaebb8332af
Jacøb <85955074+cuber1515@users.noreply.github.com>
2023-12-20 15:29:22 -0800

    Add files via upload

--- autoTest.java
+++ autoTest.java
@@ -1,102 +1,267 @@
-//package org.firstinspires.ftc.teamcode;
-//
-//import android.graphics.Bitmap;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//
-//import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-//import org.opencv.core.Core;
-//import org.opencv.core.Mat;
-//import org.opencv.core.Scalar;
-//import org.opencv.core.Rect;
-//import org.opencv.imgproc.Imgproc;
-//import org.openftc.easyopencv.OpenCvCamera;
-//import org.openftc.easyopencv.OpenCvCameraFactory;
-//import org.openftc.easyopencv.OpenCvCameraRotation;
-//import org.openftc.easyopencv.OpenCvPipeline;
-//import org.openftc.easyopencv.OpenCvWebcam;
-//import org.openftc.easyopencv.PipelineRecordingParameters;
-//
-//@Autonomous
-//public class autoTest extends LinearOpModeOpMode {
-//    OpenCvWebcam webcam = null;
-//
-//    @Override
-//    public void init() {
-//        WebcamName webcamName = hardwareMap.get(WebcamName.class, "name");
-//        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-//        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
-//
-//        webcam.setPipeline(new colorPipeline());
-//
-//        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-//            @Override
-//            public void onOpened() {
-//                webcam.startStreaming(640, 360, OpenCvCameraRotation.UPRIGHT);
-//            }
-//
-//            @Override
-//            public void onError(int errorCode) {
-//
-//            }
-//        });
-//
-//        public void runOpMode() throws InterruptedException {
-//
-//        }
-//    }
-//
-//    @Override
-//    public void loop() {
-//
-//    }
-//
-//    class colorPipeline extends OpenCvPipeline {
-//        Mat YCbCr = new Mat();
-//        Mat leftCrop;
-//        Mat rightCrop;
-//        double leftavgfin;
-//        double rightavgfin;
-//        Mat outPut = new Mat();
-//        Scalar rectColor = new Scalar(0.0, 0.0, 255.0);
-//
-//        public Mat processFrame(Mat input) {
-//            Imgproc.cvtColor(input, YCbCr, Imgproc.COLOR_RGB2YCrCb);
-//            telemetry.addLine("pipeline running");
-//
-//            Rect leftRect = new Rect(1, 1, 300, 359);
-//            Rect rightRect = new Rect(339, 1, 300, 359);
-//
-//            input.copyTo(outPut);
-//            Imgproc.rectangle(outPut, leftRect, rectColor, 2);
-//            Imgproc.rectangle(outPut, rightRect, rectColor, 2);
-//
-//            leftCrop = YCbCr.submat(leftRect);
-//            rightCrop = YCbCr.submat(rightRect);
-//
-//            Core.extractChannel(leftCrop, leftCrop, 2);
-//            Core.extractChannel(rightCrop, rightCrop, 2);
-//
-//            Scalar leftavg = Core.mean(leftCrop);
-//            Scalar rightavg = Core.mean(rightCrop);
-//
-//            leftavgfin = leftavg.val[0];
-//            rightavgfin = rightavg.val[0];
-//
-//            if (leftavgfin > rightavgfin) {
-//                telemetry.addLine("It is on the left side");
-//            } else if (rightavgfin > leftavgfin) {
-//                telemetry.addLine("It is on the right side");
-//            } else {
-//                telemetry.addLine("It must be in the middle");
-//            }
-//
-//            return outPut;
-//        }
-//
-//    }
-//
-//}
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Bitmap;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+import org.openftc.easyopencv.PipelineRecordingParameters;
+
+@Autonomous
+public class autoTest extends LinearOpMode {
+    DcMotor FR, FL, BR, BL;
+    OpenCvWebcam webcam = null;
+
+    public void startEncoders() {                                             /** start encoders **/
+        FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void exitEncoders() {                                               /** exit encoders **/
+        FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+    public void resetEncoders() {                                           /** restart encoders **/
+        FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void goForward(int targetToPlace) {                             /** Go Forward Method **/
+        resetEncoders();
+        startEncoders();
+
+        FL.setTargetPosition(targetToPlace);
+        FR.setTargetPosition(targetToPlace);
+        BL.setTargetPosition(targetToPlace);
+        BR.setTargetPosition(targetToPlace);
+
+        FL.setPower(0.5);
+        FR.setPower(0.5);
+        BL.setPower(0.5);
+        BR.setPower(0.5);
+
+        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while (FR.isBusy() || FL.isBusy() || BR.isBusy() || BL.isBusy()) {
+        }
+
+        FL.setPower(0);
+        FR.setPower(0);
+        BL.setPower(0);
+        BR.setPower(0);
+
+        exitEncoders();
+        resetEncoders();
+    }
+
+    public void turn(int tIme, String direction) {
+        if (direction == "right") {
+            FL.setPower(0.5);
+            BL.setPower(0.5);
+            FR.setPower(-0.5);
+            BR.setPower(-0.5);
+        } else if (direction == "left") {
+            FR.setPower(0.5);
+            BR.setPower(0.5);
+            FL.setPower(-0.5);
+            BL.setPower(-0.5);
+        }
+
+        if (tIme != 0) { // IF THIS IS SET TO ANY NUMBER OTHER THAN 0 IT WILL MAKE IT SO THAT IT
+            sleep(tIme); // TURNS FOR HOWEVER LONG IT IS SET TO, IF IT'S 0 IT WILL JUST SET
+            // THE MOTORS TO THE POWER INDEFINITELY
+            FR.setPower(0);
+            FL.setPower(0);
+            BR.setPower(0);
+            BL.setPower(0);
+        }
+    }
+
+    /**
+     * slide right/left
+     *
+     * example: slide(1500, left); will slide to the left at 50% power for 1.5 seconds
+     */
+    public void slide(String direction, int targetToPlace) {
+        if (direction == "right") {
+            resetEncoders();
+            startEncoders();
+
+            FL.setTargetPosition(targetToPlace);
+            FR.setTargetPosition(-targetToPlace);
+            BL.setTargetPosition(-targetToPlace);
+            BR.setTargetPosition(targetToPlace);
+
+            FL.setPower(0.5);
+            FR.setPower(0.5);
+            BL.setPower(0.5);
+            BR.setPower(0.5);
+
+            FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            while (FR.isBusy() || FL.isBusy() || BR.isBusy() || BL.isBusy()) {
+            }
+
+            FL.setPower(0);
+            FR.setPower(0);
+            BL.setPower(0);
+            BR.setPower(0);
+            exitEncoders();
+            resetEncoders();
+        } else if (direction == "left") {
+            resetEncoders();
+            startEncoders();
+
+            FL.setTargetPosition(-targetToPlace);
+            FR.setTargetPosition(targetToPlace);
+            BL.setTargetPosition(targetToPlace);
+            BR.setTargetPosition(-targetToPlace);
+
+            FL.setPower(0.5);
+            FR.setPower(0.5);
+            BL.setPower(0.5);
+            BR.setPower(0.5);
+
+            FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            while (FR.isBusy() || FL.isBusy() || BR.isBusy() || BL.isBusy()) {
+            }
+
+            FL.setPower(0);
+            FR.setPower(0);
+            BL.setPower(0);
+            BR.setPower(0);
+            exitEncoders();
+            resetEncoders();
+        }
+    }
+
+    public int convert(double inch) {
+        double newinch = inch * -42.75; //1026 is the value for 24 inches, aka 2 feet
+        return ((int)newinch);
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        FR = hardwareMap.dcMotor.get("rightFront");
+        FL = hardwareMap.dcMotor.get("Buh");
+        BR = hardwareMap.dcMotor.get("rightBack");
+        BL = hardwareMap.dcMotor.get("Bruh");
+
+        FR.setDirection((DcMotor.Direction.REVERSE));
+        BR.setDirection(DcMotor.Direction.REVERSE);
+
+
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+
+        webcam.setPipeline(new colorPipeline());
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(640, 360, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+
+        waitForStart();
+
+
+    }
+    class colorPipeline extends OpenCvPipeline {
+        Mat YCbCr = new Mat();
+        Mat leftCrop;
+        Mat rightCrop;
+        double leftavgfin;
+        double rightavgfin;
+        Mat outPut = new Mat();
+        Scalar rectColor = new Scalar(0.0, 0.0, 180.0);
+
+        public Mat processFrame(Mat input) {
+            Imgproc.cvtColor(input, YCbCr, Imgproc.COLOR_RGB2YCrCb);
+            telemetry.addLine("pipeline running");
+
+            Rect leftRect = new Rect(1, 1, 300, 359);
+            Rect rightRect = new Rect(339, 1, 300, 359);
+
+            input.copyTo(outPut);
+            Imgproc.rectangle(outPut, leftRect, rectColor, 2);
+            Imgproc.rectangle(outPut, rightRect, rectColor, 2);
+
+            leftCrop = YCbCr.submat(leftRect);
+            rightCrop = YCbCr.submat(rightRect);
+
+            Core.extractChannel(leftCrop, leftCrop, 2);
+            Core.extractChannel(rightCrop, rightCrop, 2);
+
+            Scalar leftavg = Core.mean(leftCrop);
+            Scalar rightavg = Core.mean(rightCrop);
+
+            leftavgfin = leftavg.val[0];
+            rightavgfin = rightavg.val[0];
+
+            int spikePlacement;
+
+            if (leftavgfin > rightavgfin) {
+                telemetry.addLine("It is on the left side");
+                spikePlacement = 0;
+            } else if (rightavgfin > leftavgfin) {
+                telemetry.addLine("It is on the right side");
+                spikePlacement = 1;
+            } else {
+                telemetry.addLine("It must be in the middle");
+                spikePlacement = 2;
+            }
+            telemetry.update();
+
+
+            int x = convert(36);
+            slide("left",x);
+
+            if (spikePlacement == 0) {
+                turn(2000, "left");
+            } else if (spikePlacement == 2) {
+                turn(2000, "right");
+            } else {
+            }
+
+            return outPut;
+        }
+
+    }
+
+}
--- teleOp/teleOpCodeLogan.java
+++ teleOp/teleOpCodeLogan.java
@@ -39,7 +39,7 @@ public class teleOpCodeLogan extends LinearOpMode {
         Claw = hardwareMap.servo.get("Claw");
 
         FR.setDirection((DcMotorSimple.Direction.REVERSE));
-        //BR.setDirection((DcMotorSimple.Direction.REVERSE));
+        BR.setDirection((DcMotorSimple.Direction.REVERSE));
 
         telemetry.addData(">", "Press Play to start op mode"); // Will add stuff to the driver hub screen
         telemetry.update(); // Will update the driver hub screen so that the above will appear
@@ -48,81 +48,81 @@ public class teleOpCodeLogan extends LinearOpMode {
         waitForStart(); // When the start button is pressed
 
 
-            while (opModeIsActive()) {
+        while (opModeIsActive()) {
 
-                /**
-                 * ALL UNDER GAMEPAD 1
-                 */
-                // Basic robot moving controls
-                FR.setPower(gamepad1.right_stick_y * speed);
-                BR.setPower(gamepad1.right_stick_y * (speed*0.75));
-                FL.setPower(gamepad1.left_stick_y * speed);
-                BL.setPower(gamepad1.left_stick_y * (speed*2));
+            /**
+             * ALL UNDER GAMEPAD 1
+             */
+            // Basic robot moving controls
+            FR.setPower(gamepad1.right_stick_y * speed);
+            BR.setPower(gamepad1.right_stick_y * (speed*0.75));
+            FL.setPower(gamepad1.left_stick_y * speed);
+            BL.setPower(gamepad1.left_stick_y * (speed*2));
 
-                if (gamepad1.dpad_up) {
-                    AM.setPower(1);
-                } else if (gamepad1.dpad_down) {
-                    AM.setPower(-1);
+            if (gamepad1.dpad_up) {
+                AM.setPower(1);
+            } else if (gamepad1.dpad_down) {
+                AM.setPower(-1);
+            } else {
+                AM.setPower(0);
+            }
+            if (gamepad1.dpad_left) {
+                HM.setPower(0.15);
+            } else if (gamepad1.dpad_right) {
+                HM.setPower(-0.15);
+            } else {
+                HM.setPower(0);
+            }
+            //This is for strafing'
+            //THESE ARE THE OLD GAMEPAD 2 CONTROLS
+            if (gamepad1.right_trigger > 0) {
+                FR.setPower(speed);
+                BL.setPower(speed*2);
+                BR.setPower(-speed*0.75);
+                FL.setPower(-speed);
+            } else {
+                if (gamepad1.left_trigger > 0) {
+                    FR.setPower(-speed);
+                    BL.setPower(-speed*2);
+                    BR.setPower(speed*0.75);
+                    FL.setPower(speed);
                 } else {
-                    AM.setPower(0);
-                }
-                if (gamepad1.dpad_left) {
-                    HM.setPower(0.15);
-                } else if (gamepad1.dpad_right) {
-                    HM.setPower(-0.15);
-                } else {
-                    HM.setPower(0);
-                }
-                //This is for strafing'
-                //THESE ARE THE OLD GAMEPAD 2 CONTROLS
-                if (gamepad1.right_trigger > 0) {
-                    FR.setPower(speed);
-                    BL.setPower(speed*2);
-                    BR.setPower(-speed*0.75);
-                    FL.setPower(-speed);
-                } else {
-                    if (gamepad1.left_trigger > 0) {
-                        FR.setPower(-speed);
-                        BL.setPower(-speed*2);
-                        BR.setPower(speed*0.75);
-                        FL.setPower(speed);
-                    } else {
-                        FR.setPower(0);
-                        BL.setPower(0);
-                        BR.setPower(0);
-                        FL.setPower(0);
-                    }
-                }
-                CM.setPower(gamepad2.right_stick_y*halfspeed); //THESE ARE THE OLD GAMEPAD 2 CONTROLS
-                /**
-                 * ALL UNDER GAMEPAD 2
-                 */
-                /**if (gamepad1.b) {
-                    CM.setPower(halfspeed);
-                } else if (gamepad1.x) {
-                    CM.setPower(-halfspeed);
-                } else {
-                    CM.setPower(0);
-                }**/
-                SM.setPower(gamepad2.left_stick_y * -speed); //THESE ARE THE  OLD GAMEPAD 2 CONTROLS
-                /**if (gamepad1.y) {
-                    encoder(2);
-//                    SM.setPower(speed);
-                }
-
-                if (gamepad1.a) {
-                    SM.setPower(-speed);
-                } else {
-                    SM.setPower(0);
-                }**/
-                if (gamepad2.left_bumper) {         //gamepad1.left_trigger > 0 OLD GAMEPAD 2 CONTROLS
-                    Claw.setPosition(openClaw);
-                }
-                if (gamepad2.right_bumper) {       //gamepad1.right_bumper > 0  OLD GAMEPAD 2 CONTROLS
-                    Claw.setPosition(closeClaw);
+                    FR.setPower(0);
+                    BL.setPower(0);
+                    BR.setPower(0);
+                    FL.setPower(0);
                 }
             }
+            CM.setPower(gamepad2.right_stick_y*halfspeed); //THESE ARE THE OLD GAMEPAD 2 CONTROLS
+            /**
+             * ALL UNDER GAMEPAD 2
+             */
+            /*if (gamepad1.b) {
+             CM.setPower(halfspeed);
+             } else if (gamepad1.x) {
+             CM.setPower(-halfspeed);
+             } else {
+             CM.setPower(0);
+             }*/
+            SM.setPower(gamepad2.left_stick_y * -speed); //THESE ARE THE  OLD GAMEPAD 2 CONTROLS
+            if (gamepad1.y) {
+                encoder(2);
+                SM.setPower(speed);
+            }
+
+             if (gamepad1.a) {
+                 SM.setPower(-speed);
+             } else {
+                 SM.setPower(0);
+             }
+            if (gamepad2.left_bumper) {         //gamepad1.left_trigger > 0 OLD GAMEPAD 2 CONTROLS
+                Claw.setPosition(openClaw);
+            }
+            if (gamepad2.right_bumper) {       //gamepad1.right_bumper > 0  OLD GAMEPAD 2 CONTROLS
+                Claw.setPosition(closeClaw);
+            }
         }
+    }
 
     public void encoder(int a){
 //        SM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
